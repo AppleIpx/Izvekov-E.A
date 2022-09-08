0e36e2dd65f7f7fa4af71b4bd8d946596f585949
AppleIpx <zhenya.0295@mail.ru>
2022-09-08 18:47:52 +0300

    Initial commit

---
+++ task3.py
@@ -0,0 +1,15 @@
+name = input('Как вас зовут')
+age = int(input('Введите ваш возраст'))
+if 0 < age < 75:
+    if age >= 16 and name != 'Иван':
+        print('Поздравляем вы поступили в ВГУИТ')
+    else:
+        print('Извините, вы не поступили')
+        if age < 16:
+            remainder = 16 - age
+            if remainder < 5:
+                print('Вначале окончите школу, вам осталось', remainder, 'год(а)', sep=' ')
+            else:
+                print('Вначале окончите школу, вам осталось', remainder, 'лет', sep=' ')
+else:
+    print('Ошибка')
---
+++ task4.py
@@ -0,0 +1,6 @@
+seconds = input()
+minutes = seconds / 60
+seconds = seconds % 60
+hours = minutes / 60
+days = hours / 24
+print('Дней=', days, 'часов=', hours, 'минут=', minutes, 'секунд=', seconds, sep=(" "))
